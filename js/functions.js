ef7c2bd3a089be7293bc1c28a81b6b5d36bda809
Frank Candamil <fcandamil@gmail.com>
2013-10-13 23:07:26 -0400

    Set max width to slider. Image only made for 1012px.

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,34 @@
-jQuery(function($) {
-	$( "div ul li a.lb_logos,div ul li a.lb_mobile").rlightbox({counterDelimiter: " of "});
-	
+$(document).ready(function() {
+
+var $window = $(window); 
+
+	function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize >= 440) {
+       		 $("div ul li a.lb_logos,div ul li a.lb_mobile").rlightbox({counterDelimiter: " of "});
+		}
+		else if(windowsize <= 439) {
+			$("div ul li a").removeClass("lb_logos","lb_mobile");
+		}
+		
+		
+	}
+
+checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth)
+});
+
+$(document).ready(function() {
 	$("div#websites-row div:nth-child(2n+1),div#mobile-row div:nth-child(2n+1), div#identity-row div:nth-child(2n+1)").css("float", "right");
-	
+});	
+
+$(document).ready(function() {	
 	$('.bxslider').bxSlider({
   mode: 'vertical',
   slideMargin: 0
 	});
-	
-});
+});		
   
 
    
